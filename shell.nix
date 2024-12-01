1e2465430dea7250fc9dc0917a6ba8a04158e42d
Arqam Zia <arqam.mzia@gmail.com>
2024-12-01 18:13:54 +0500

    Create an exhaustive shell.nix

---
+++ shell.nix
@@ -0,0 +1,42 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+let
+  # Haskell environment setup
+  haskellEnv = pkgs.ghc;
+
+  # Elixir environment setup
+  elixirEnv = pkgs.elixir_1_15;
+
+  # Python environment setup
+  pythonEnv = pkgs.python312Full;
+
+  # C++ / C development tools setup
+  cppEnv = pkgs.libgcc;
+  cppTools = pkgs.gnumake;
+  clangEnv = pkgs.libclang;
+
+in pkgs.mkShell rec {
+  name = "advent-of-code-env";
+
+  buildInputs = [
+    haskellEnv
+    elixirEnv
+    pythonEnv
+    cppEnv
+    cppTools
+    clangEnv
+  ];
+
+  # Optional, for use with specific tools or dependencies
+  shellHook = ''
+    echo "Welcome to the Advent of Code environment!"
+    echo "Available languages: Elixir, Haskell, Python, C++, C"
+    echo "Use 'ghc', 'elixir', 'python3', 'gcc', or 'clang' to start coding!"
+  '';
+
+#   # Add useful environment variables (optional)
+#   shellInit = ''
+#     export LANG="en_US.UTF-8"
+#     export PATH="$PATH:${elixirEnv}/bin:${haskellEnv}/bin:${pythonEnv}/bin:${cppEnv}/bin"
+#   '';
+}
